28a2a230f5c74213500fced46ee5fda39a4c98db
Singh_ Veenu (veesingh@deloitte.com) <veesingh@your.hostname.com>
2023-05-26 14:48:44 +0530

    header-styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,20 +2,35 @@ import React from 'react';
 import {ReactComponent as NotifiactionLogo} from "../../assets/icons/Vector.svg"
 import "./header.scss"
 import { NavLink } from 'react-router-dom';
+import { ReactComponent as SearchLogo } from "../../assets/icons/search.svg";
+
+import  profilePhoto from "../../assets/images/profile-photo.svg";
+import Logo from '../Logo/Logo';
 
 const Header = () => {
     return (
         <div className='d-flex justify-content-between w-100 align-items-end header-container'>
-            <h2>BingeBox</h2>
+            <Logo/>
             <div className='d-flex justify-content-between w-30'>
-               <NavLink href="/movies">Movies</NavLink>
-               <NavLink href="/series">Series</NavLink>
-               <NavLink href="/tv-shows">TV Shows</NavLink>
-               <NavLink href="/anime">Anime</NavLink>
+               <NavLink to="/movies" className={({ isActive }) => (isActive ? "header-link-active-style" : "")}>Movies</NavLink>
+               <NavLink to="/series" className={({ isActive }) => (isActive ? "header-link-active-style" : "")}>Series</NavLink>
+               <NavLink to="/tv-shows" className={({ isActive }) => (isActive ? "header-link-active-style" : "")}>TV Shows</NavLink>
+               <NavLink to="/anime" className={({ isActive }) => (isActive ? "header-link-active-style" : "")}>Anime</NavLink>
             </div>
-            <div>
-             <input type="text" placeholder='search here'/>
-             <NotifiactionLogo/>
+            <div className='d-flex  w-30'>
+                <div className='d-flex align-items-end'>
+                    <SearchLogo width="1.3rem" height="1.3rem"/>
+
+                <input type="text" placeholder='search here'/>
+
+                </div>
+                <div className='d-flex justify-content-center align-items-center last-container'>
+                <NotifiactionLogo width="1.1rem" height="1.3rem" className='noti-icon'/>
+             <img src={profilePhoto} className='profile-style'/>
+
+                </div>
+             
+             
 
             </div>
         </div>
