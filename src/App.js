0344a76d36d2a03b022d2a3a0aa129c0fc70b66f
Ritik Mittal <rimittal@deloitte.com>
2023-06-01 19:09:12 +0530

    merged

--- src/App.js
+++ src/App.js
@@ -7,29 +7,34 @@ import Home from "./components/Home/Home";
 import WishList from "./components/WishList/WishList";
 import { useState } from "react";
 import Description from "./components/Description/Description";
-import { UserContextProvider } from "./context/Context/UserContext/UserState"; 
+import { UserContextProvider } from "./context/Context/UserContext/UserState";
+import UserPreferences from "./components/UserPreferences/UserPreferences";
+import Profile from "./components/Profile/Profile";
+import SearchPage from "./components/Searchpage/SearchPage";
 
 function App() {
-  const [isLogin,setIsLogin]=useState(false)
-  console.log("islogin",isLogin)
-  
+  const [isLogin, setIsLogin] = useState(false);
+  console.log("islogin", isLogin);
+
   return (
     <div className="App">
-  <UserContextProvider>
-      <Routes>
-        
-        
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login setIsLogin={setIsLogin}/>} />
-        {
-          (<Route element={<AppLayout/>}>
-          <Route path="/home" element={<Home/>} />      
-          <Route path="/wishlist" element={<WishList/>} />   
-          <Route path="/description/:movieId" element={<Description/>}/>                                                                                                                                                                             
-          </Route>)
-        }
-        
-      </Routes>
+      <UserContextProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login setIsLogin={setIsLogin} />} />
+          <Route path="/userpreferences" element={<UserPreferences />} />
+
+          {
+            <Route element={<AppLayout />}>
+              <Route path="/home" element={<Home />} />
+
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/searchresults" element={<SearchPage />} />
+              <Route path="/wishlist" element={<WishList />} />
+              <Route path="/description/:movieId" element={<Description />} />
+            </Route>
+          }
+        </Routes>
       </UserContextProvider>
     </div>
   );
