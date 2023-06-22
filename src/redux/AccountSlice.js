a16a8214334403c13381e8ab27423517a4a6e6c9
Shakya <ashakya@deloitte.com>
2023-06-22 12:01:27 +0530

    mock data change

--- src/redux/AccountSlice.js
+++ src/redux/AccountSlice.js
@@ -1,21 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
-const AccountSlice=createSlice({
-name:"Accounts",
-initialState:{currentAccount:{ },userDetails:{}},
-reducers:{
-selectCurrentAccount:(state,action)=>{
-    
-    state.currentAccount=action.payload
-    console.log("yes",action,"state",state.currentAccount)
-},
-saveSignUpInfo:(state,action)=>{
-    state.userDetails=action.payload
-    state.currentAccount=action.payload.accounts[0]
-},
-addAccount:(state,action)=>{
-    state.userDetails?.accounts?.push(action.payload)
-}
-}
+const AccountSlice = createSlice({
+    name: "Accounts",
+    initialState: { currentAccount: {}, userDetails: {} },
+    reducers: {
+        selectCurrentAccount: (state, action) => {
+
+            state.currentAccount = action.payload
+            console.log("yes", action, "state", state.currentAccount)
+        },
+        saveSignUpInfo: (state, action) => {
+            state.userDetails = action.payload
+            state.currentAccount = action.payload.accounts[0]
+        },
+        addAccount: (state, action) => {
+            state.userDetails?.accounts?.push(action.payload)
+        }
+    }
 })
 export default AccountSlice.reducer
-export const AccountAction=AccountSlice.actions
+export const AccountAction = AccountSlice.actions
